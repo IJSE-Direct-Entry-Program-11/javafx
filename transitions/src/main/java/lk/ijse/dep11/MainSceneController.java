c4658f65ac398520a45968ae799716238c651897
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2023-08-21 10:52:12 +0530

    Add ids and event listeners

--- transitions/src/main/java/lk/ijse/dep11/MainSceneController.java
+++ transitions/src/main/java/lk/ijse/dep11/MainSceneController.java
@@ -1,4 +1,19 @@
 package lk.ijse.dep11;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Circle;
+
 public class MainSceneController {
+    public Button btnFadeIn;
+    public Button btnFadeOut;
+    public Circle shpFade;
+    public AnchorPane root;
+
+    public void btnFadeInOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnFadeOutOnAction(ActionEvent actionEvent) {
+    }
 }
