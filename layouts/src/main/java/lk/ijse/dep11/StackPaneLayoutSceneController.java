053f9c2568bdc8b79a9f2a1f62af550ccede84c9
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2023-08-23 00:58:41 +0530

    Finalize the layouts

--- layouts/src/main/java/lk/ijse/dep11/StackPaneLayoutSceneController.java
+++ layouts/src/main/java/lk/ijse/dep11/StackPaneLayoutSceneController.java
@@ -3,12 +3,26 @@ package lk.ijse.dep11;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Rectangle;
 
 public class StackPaneLayoutSceneController {
     public Circle shpCircle;
     public Button btnBringToFront;
+    public Rectangle shpRectangle;
+    public Polygon shpPoly;
+
+    boolean front = false;
+
+    public void initialize(){
+        shpRectangle.setViewOrder(1);
+        shpPoly.setViewOrder(1);
+        shpCircle.setViewOrder(2);
+    }
 
     public void btnBringToFrontOnAction(ActionEvent actionEvent) {
-        shpCircle.toFront();
+//        shpCircle.toFront();
+//        shpCircle.toBack();
+        shpCircle.setViewOrder((front = !front) ? 0 : 2);
     }
 }
